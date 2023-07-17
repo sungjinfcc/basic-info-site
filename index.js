8a2129fd1937c6e8ca1b94d9ec3c786433ffdc11
sungjin <ysoon0912@gmail.com>
2023-07-17 15:01:21 +0900

    Add express

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-  let filePath = '';
-  switch (req.url) {
-    case '/':
-      filePath = path.join(__dirname,'index.html');
-      break;
-    case '/about':
-      filePath = path.join(__dirname,'about.html');
-      break;
-    case '/contact-me':
-      filePath = path.join(__dirname,'contact-me.html');
-      break;
-    default:
-      filePath = path.join(__dirname,'404.html');
-      res.statusCode = 404; 
-      break;
-  }
-
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      console.error(err);
-      res.statusCode = 500;
-      res.end('Internal Server Error');
-    } else {
-      res.setHeader('Content-Type', 'text/html');
-      res.end(content);
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+app.get("*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
